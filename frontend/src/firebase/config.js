853737089053f50016f5ba55b4dd2fc1e289f8cb
Rethin-2000 <rethinfrancis@gmail.com>
2025-07-30 20:17:24 +0530

    changes added

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -5,15 +5,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import dotenv from 'dotenv';
-import meta from 'meta';
+
 dotenv.config();
 const firebaseConfig = {
-  apiKey: meta.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: meta.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
